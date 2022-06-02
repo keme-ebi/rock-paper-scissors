0cf2b7667bbf221bbdcb4e24d41fdcdae79d3885
keme-ebi <kelvin6bolou@gmail.com>
2022-06-02 23:50:56 +0100

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 # rules of the game............
-print("Welcome to a guessing game of rock, paper, scissors. \n" +
+print("Welcome to a game of rock, paper, scissors. \n" +
       "How to win: \n" + "Rock vs Paper: Paper wins. \n" + "Rock vs Scissors: Rock wins. \n" + "Paper vs Scissors: Scissors wins. \n")
 while True:
     options = ["rock", "paper", "scissors"]
@@ -12,19 +12,23 @@ while True:
         player = input("rock, paper, or scissors? ").lower()
     # conditions for the game........
     if player == computer:
-        print("computer: ", (computer), ":", "player: ", (player))
+        print("computer", "(" + computer + ")",
+              ":", "player", "(" + player + ")")
         print("draw, same guess")
     elif player == "rock":
         if computer == "paper":
-            print("computer: ", (computer), ":", "player: ", (player))
+            print("computer", "(" + computer + ")",
+                  ":", "player", "(" + player + ")")
             print("you win! paper covers rock... ")
     elif player == "paper":
         if computer == "scissors":
-            print("computer: ", (computer), ":", "player: ", (player))
+            print("computer", "(" + computer + ")",
+                  ":", "player", "(" + player + ")")
             print("you lose! scissors cuts paper... ")
     elif player == "scissors":
         if computer == "paper":
-            print("computer: ", (computer), ":", "player: ", (player))
+            print("computer", "(" + computer + ")",
+                  ":", "player", "(" + player + ")")
             print("you win! scissors cuts paper... ")
     # condition to play again..........
     play_again = input("Would you like to play again? (yes/no): ").lower()
